5204bc9de76c067d8b728bb8dcb5f954b2ab453d
András Urbin <urbinandras@gmail.com>
2018-11-22 09:32:51 +0100

    shout

--- gittie.py
+++ gittie.py
@@ -1,11 +1,8 @@
 def introduce():
     print("Hello, I'm Gittie!")
-<<<<<<< HEAD
     print("I like cheese! c: ")
-=======
     print("Kecske")
 
->>>>>>> 85dc92f687442417f0bb355646ca486f51a2710c
 
 def add():
     a = 2
@@ -23,4 +20,9 @@ def joke():
     print("You think you are funny? -_-")
 
 
+def shout():
+    print("SHOUTIIIIIING!")
+
+
+shout()
 joke()
